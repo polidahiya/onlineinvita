ffbab7f7e6e3bb881f156f1d08f7bdce96189f55
Parvesh <121171390+polidahiya@users.noreply.github.com>
2023-12-21 17:14:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const ObjectId = require("mongodb").ObjectId;
 const mongoose = require("mongoose"); //install mongoose my command "npm i mongoose"
 const { MongoClient } = require("mongodb");
+const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.listen(3005);
-require("dotenv").config();
-
+//
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 //
 app.use(express.static("./build"));
 
-const db_link = process.env.DATABASE_URL;
+const db_link =
+  "mongodb+srv://polidahiya830:12er56ui90%40Poli@cluster0.pvrgiqn.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(db_link).then(async function () {
   const client = new MongoClient(db_link);
@@ -24,6 +28,7 @@ mongoose.connect(db_link).then(async function () {
 
   console.log("listening");
   // get previewdata
+
   app.get("/previewdata", async (req, res) => {
     try {
       let search;
@@ -40,17 +45,31 @@ mongoose.connect(db_link).then(async function () {
   });
 
   // createposts
-  app.post("/savecard", verifyToken, async (req, res) => {
-    try {
-      let result = await cards.insertOne(req.body);
-      res.json({
-        message: "Saved Successfully",
-        id: result.insertedId,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  const storage = multer.memoryStorage();
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fieldSize: 10 * 1024 * 1024,
+      fileSize: 10 * 1024 * 1024,
+    },
   });
+  app.post(
+    "/savecard",
+    verifyToken,
+    upload.single("data"),
+    async (req, res) => {
+      try {
+        const formData = JSON.parse(req.body.data);
+        let result = await cards.insertOne(formData);
+        res.json({
+          message: "Saved Successfully",
+          id: result.insertedId,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
 
   //cards
   app.get("/task", verifyToken, async (req, res) => {
@@ -193,5 +212,47 @@ mongoose.connect(db_link).then(async function () {
       console.log("unlogined request");
       return res.json({ message: "Please login first" });
     }
+  }
+});
+
+// test
+// 1BBa_ID-1qsu7nMZpNt6QbK5cKON8y6xs
+const { google } = require("googleapis");
+// const { JWT } = require("google-auth-library");
+
+const keyFilePath = require("./online-invitation-storage-47651094aabd.json");
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: keyFilePath,
+  scopes: ["https://www.googleapis.com/auth/drive"],
+});
+
+const drive = google.drive({ version: "v3", auth });
+app.post("/upload", async (req, res) => {
+  console.log(req.body);
+  try {
+    const base64Data = req.body.imagedata;
+    const tempImagePath = "./image.jpg";
+    base64Img.imgSync(base64Data, tempImagePath);
+
+    // Upload the image to Google Drive
+    const response = await drive.files.create({
+      requestBody: {
+        name: "uploaded_image.jpg", // Set the desired file name
+        mimeType: "image/jpeg",
+      },
+      media: {
+        mimeType: "image/jpeg",
+        body: require("fs").createReadStream(tempImagePath),
+      },
+    });
+
+    // Delete the temporary file
+    require("fs").unlinkSync(tempImagePath);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
